0ef0e243e4e073e03273f0c08437ae763860de23
Gnel12345 <gnel1234@yahoo.com>
2021-11-07 15:29:43 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@
 import './App.css';
 import Row from"./Row.js";
 import requests from './request';
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 
 
 function App() {
   return (
     <div className="App">
-     <Row title="NETFLIX ORIGINALS" fetchUrl = {requests.fetchNetflixOriginals}/>
+      <Nav/>
+      <Banner/>
+      
+     <Row title="NETFLIX ORIGINALS" fetchUrl = {requests.fetchNetflixOriginals}
+     isLargeRow/>
      <Row title="TRENDING NOW" fetchUrl = {requests.fetchTrending}/>
+     <Row title="Top Rated" fetchUrl = {requests.fetchTopRated}/>
+     <Row title="Action Movies" fetchUrl = {requests.fetchActionMovies}/>
+     <Row title="Comedy Movies" fetchUrl = {requests.fetchComedyMovies}/>
+     <Row title="Horror Movies" fetchUrl = {requests.fetchActionMovies}/>
+     <Row title="Romance Movies" fetchUrl = {requests.fetchComedyMovies}/>
+     <Row title="Documentaries" fetchUrl = {requests.fetchTopRated}/>
+     
+
     </div>
   );
 }
